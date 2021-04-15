a432cef917c9570332eaa2794c05caf27bf83cc7
Matthew Murphy <murphy@mmurphy.local>
2021-04-15 16:04:35 -0400

    remove unnecessary files

--- scripts/git_diff.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-path=$1
-base_file=$2
-new_file=$5
-
-printf '%s\n' $path
-diff -b -B $base_file $new_file | grep -v '^[<>-]'
-exit 0
--- src/runner.js
+++
@@ -1,4 +0,0 @@
-const shell = require('shelljs')
-const path = require('path')
-
-shell.exec(path.join(__dirname, '..', 'scripts', 'runner.sh'))
